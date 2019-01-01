0f3c0afada4598972f6fb9711286a33d589e0c86
Phil Hartley <philh725@gmail.com>
2019-01-01 20:00:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(file_path)
   emoticons = YAML.load_file(file_path)
   library = {:get_meaning => {}, :get_emoticon => {} }
   emoticons.each do |meaning, emote_array|
-    library[:get_meaning] = {emote_array[0] => meaning}
+    library[:get_meaning][emote_array[0]] = meaning}
     library[:get_emoticon] = {emote_array[0] => emote_array[1]}
   end
   puts library
