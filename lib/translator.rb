a33c6c1525eda6f43ee3f15411d430102d43b138
Phil Hartley <philh725@gmail.com>
2019-01-01 20:01:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ def load_library(file_path)
   library = {:get_meaning => {}, :get_emoticon => {} }
   emoticons.each do |meaning, emote_array|
     library[:get_meaning][emote_array[0]] = meaning
-    library[:get_emoticon] = {emote_array[0] => emote_array[1]}
+    library[:get_emoticon][emote_array[0]] = emote_array[1]
   end
   puts library
   library
